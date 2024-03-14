e10070e62471c0687fcc5b918e4420294fa3debc
Carlos Kirui <carloskirui154@gmail.com>
2024-03-14 06:05:38 -0400

    adjusted css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
         <div>
-            <nav className="bg-gradient-to-r from-purple-800 via-violet-900 to-purple-800 p-4 text-white">
+            <nav className="bg-gradient-to-r from-blue-800 via-blue-900 to-purple-800 p-4 text-white">
                 <div className="container mx-auto flex items-center justify-between">
                     <div className="flex items-center space-x-2">
                         <span className="text-lg font-bold">Base64Tool</span>
